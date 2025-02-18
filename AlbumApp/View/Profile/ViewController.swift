54fc2d10e9d5ad48e8c6112a98b3234ab0745309
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-02-18 09:59:02 +0200

    add the init storyboard structure

--- AlbumApp/View/Profile/ViewController.swift
+++ AlbumApp/View/Profile/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var albumsTableView: UITableView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
