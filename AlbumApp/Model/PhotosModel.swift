85c31aa9e4dc51af6caadcf3e0dbf09a5b235211
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-02-18 10:08:31 +0200

    Albums Model after parsing api on json fotmatter

---
+++ AlbumApp/Model/PhotosModel.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosModel.swift
+//  AlbumApp
+//
+//  Created by Usef on 18/02/2025.
+//
+
+import Foundation
---
+++ AlbumApp/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  AlbumApp
+//
+//  Created by Usef on 18/02/2025.
+//
+
+import Foundation
