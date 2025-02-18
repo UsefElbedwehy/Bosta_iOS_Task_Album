8aee3b9c2a38eefb5be5c3861040bd2aeac3ff1a
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-02-18 10:08:52 +0200

    model

--- AlbumApp/Model/PhotosModel.swift
+++ AlbumApp/Model/PhotosModel.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct PhotosModel: Codable {
+    var albumId: Int
+    var id: Int
+    var title: String
+    var url: String
+    var thumbnailUrl: String
+}
--- AlbumApp/Model/UserModel.swift
+++ AlbumApp/Model/UserModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct UserModel: Codable {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String?
+    let suite: String?
+    let city: String?
+    let zipcode: String?
+    let geo: Geo?
+}
+
+struct Geo: Codable {
+    let lat: String?
+    let lng: String?
+}
